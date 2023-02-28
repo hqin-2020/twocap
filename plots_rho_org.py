b371b643231add5803c68474172e2790a59092a0
hqin-2020 <uctphqi@ucl.ac.uk>
2023-02-28 16:46:54 -0600

    new commit

--- plots_rho_org.py
+++ plots_rho_org.py
@@ -102,12 +102,9 @@ plt.close()
 fig, ax = plt.subplots(1,1,figsize = (4,4))
 g_Z = g.sum(axis=0)*0.036
 g_Zb = gb.sum(axis=0)*0.036
-newinterval = trans(np.linspace(-1,1,201))[1:] - trans(np.linspace(-1,1,201))[:-1]
-g_Z = (g_Z*0.01).iloc[1:]/newinterval
-g_Zb = (g_Zb*0.01).iloc[1:]/newinterval
 sns.lineplot(data = g_Z,label = r"$g_Z$")
 sns.lineplot(data = g_Zb,label = r"$g_Z, \rho =1$", ls = '--')
-ax.set_ylim([0.0,5.0])
+ax.set_ylim([0.0,3.0])
 ax.set_ylabel(r'$g_Z$')
 ax.set_xlabel(r'$Z$')
 ax.set_title(r'Z density, '+ '$\gamma=$'+str(gamma)+', '+'$\\rho$ ='+str(rho))
