a6b01fb8dc68cd1cbdcebb2ed6b06e35e441eef5
hqin-2020 <uctphqi@ucl.ac.uk>
2023-02-28 23:50:51 -0600

    new commit

--- run_rho_eq.sh
+++ run_rho_eq.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-Deltaarray=(100 500 1000)
+Deltaarray=(100 500 1000 5000 10000)
 # fractionarray=(0.1 0.05 0.01 0.005 0.001 0.0005 0.0001)
 fractionarray=(0.0)
 
@@ -10,8 +10,7 @@ julia_name="newsets_twocapitals_rhoeq.jl"
 
 rhoarray=(0.7 0.8 0.9 1.00001 1.1 1.2 1.3 1.4 1.5)
 
-# gammaarray=(1.00001 2.0 3.0 5.0)
-gammaarray=(4.0 6.0 8.0)
+gammaarray=(1.00001 2.0 3.0 5.0 8.0)
 
 for Delta in ${Deltaarray[@]}; do
     for fraction in "${fractionarray[@]}"; do
@@ -19,7 +18,7 @@ for Delta in ${Deltaarray[@]}; do
             for gamma in "${gammaarray[@]}"; do
                     count=0
 
-                    action_name="TwoCapital_julia_rhoeq_time_gamma"
+                    action_name="TwoCapital_julia_rhoeq_more_test"
 
                     dataname="${action_name}_${Delta}_frac_${fraction}"
 
